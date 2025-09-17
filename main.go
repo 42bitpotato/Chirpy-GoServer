542cfb16bf126376608262b18ba4acd37adaf24b
42bitpotato <skickligt01.flagig@icloud.com>
2025-09-17 06:19:51 +0200

    initial setup

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import "net/http"
+
+func main() {
+	serverMux := http.NewServeMux()
+	server := http.Server{
+		Handler: serverMux,
+		Addr:    "8080",
+	}
+
+	server.ListenAndServe()
+	defer server.Close()
+}
